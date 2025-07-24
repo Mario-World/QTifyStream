4d26873898c377db9567799215a4e7d5b26cc9e7
Mario-World <pavansaigudelli01@gmail.com>
2025-07-24 18:00:31 +0530

    Refactor hero component and styles of hero and button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import styles from './Hero.module.css';
+import React from "react";
+import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-     
-      <div className={styles.heroImage}>
-        <img src="/assets/hero_headphones.png" alt="Headphones Illustration" />
+    <div className={styles.heroContainer}>
+      <div className={styles.textContent}>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <p>Over thousands of podcast episodes</p>
+      </div>
+      <div className={styles.imageContent}>
+        <img src="/assets/hero_headphones.png" alt="headphones" />
       </div>
     </div>
   );
 };
 
 export default Hero;
+
+
+// /assets/hero_headphones.png"
